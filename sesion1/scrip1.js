4b9b1225eef3c2424ebd2536923fca3a25becfa4
Luis Anderson Becoche Quina <quinaanderson990@gmail.com>
2026-06-17 16:43:50 -0500

    primer commit

---
+++ sesion1/scrip1.js
@@ -0,0 +1,50 @@
+console.log("Hola Mundo");
+/"sesion1: variables "/
+let edad=25; //let declara una variable
+edad=30;
+//let edad=20;
+let nombre="Andres";
+console.log(nombre);
+
+const PI=3.141598; //const mantiene el valor en todo el programa
+
+//Number numerico o decimal
+let entero=50;
+let decimal=31.40;
+let negativo=-21;
+console.log("numero entenro: ", entero); 
+
+//Boolean solo tiene 2 estados falso o verdadero
+let estadoactivo=true;
+let estadoinactivo=false;
+
+//null ausencia instrumental de valor
+const resultado=null;
+
+//undefined - variable declarada sin valor
+let sinValor;
+console.log(sinValor);
+
+// typeof - tipo de valor
+console.log(typeof -21);
+console.log(typeof "Andres");
+
+/*3 Operadores aritmeticos */
+let a = 20;
+let b = 10;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b); 
+console.log(a/b); //division
+console.log(a%b); //modulo 
+console.log(a**b); //potencia de 20 a 10
+
+//operadores comparacion
+console.log(5 == "5"); //true - igualdad de valor
+console.log(5 === "5"); //false - igualdad estricta usar siempr
+console.log(5 != "3"); //true - diferencia estricta
+console.log(10 > 5); //true
+console.log(10 <= 5); //true
+
+
